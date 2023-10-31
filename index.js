93893b393fd6a5b86cd5d19f1b6a74397e0623d0
Adrian Suarez <asuarez9382@gmail.com>
2023-10-30 21:11:24 -0700

    complete lab

--- index.js
+++ index.js
@@ -1,11 +1,33 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(ride_length){
   // Write your code here!
+  if(ride_length<= 400) {
+    return "This one is on me!";
+  } else if (ride_length < 2000) {
+    return "That will be twenty bucks."  
+  } else if (ride_length > 2500) {
+    return "No can do."
+  } else if (ride_length > 2000) {
+    return "I will gladly take your thirty bucks."
+  } 
+
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(city){
   // Write your code here!
+  if (city === 'NYC') {
+    return "Ok, sounds good.";
+  } else {
+    return "No go.";
+  }
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tip){
   // Write your code here!
+  if (tip === 'generous') {
+    return "Thank you so much.";
+  } else if (tip === 'not as generous') {
+    return 'Thank you.';
+  } else {
+    return 'Bye.';
+  }
 }
